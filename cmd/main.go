3c9243f2d15206e8920525e566c041f03ef9f5c5
Drowned Sound <carlo.c.jacinto@gmail.com>
2026-05-02 22:55:20 +0800

    Add initial task list

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+// TODO: Create basic console application
+// TODO: Create initial domain model with validation and constructors
+// TODO: Use enums and const
+// TODO: Embed structs
+// TODO: Use implicit interfaces to inject dependencies
+// TODO: Aggregate validation errors using a custom error type
+// TODO: Use sentinel errors for missing config file
+// TODO: Wrap errors
+// TODO: Use errors.Is and errors.As
+// TODO: Use defer, panic and recover
+
 func main() {
 	println("Hello, Mothership! 👾")
 }
